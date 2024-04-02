860b42d8bbeafcff991b6fe79cff6baec4167215
levipettersen <104788131+levipettersen@users.noreply.github.com>
2024-04-02 15:00:58 +0200

    Added checkbox for choosing data to plot

--- serial-monitor/src/App.js
+++ serial-monitor/src/App.js
@@ -8,9 +8,11 @@ import Plot from 'react-plotly.js';
 
 import ExportButtonCSV from './ExportButtonCSV';
 
-let socket = io('http://localhost:3001', { transports : ['websocket'] }); // Update with your server URL
+// Comment out for local testing
+// let socket = io('http://localhost:3001', { transports : ['websocket'] }); // Update with your server URL
 function connectToSocket() {
-  socket = io('http://localhost:3001', { transports : ['websocket'] });
+  // Comment out for local testing
+  // socket = io('http://localhost:3001', { transports : ['websocket'] });
 }
 
 // Ctrl+k Ctrl+, to create manual folding range
@@ -36,6 +38,8 @@ let deltaTime;
 function App() {
 
   
+
+  
   const [serialData, setSerialData] = useState({ timestamp: 0, sensor: 0, lcdButton: 0, encoderPos: 0, strainGauge: 0, pressureTransmitter: 0, deserializationError: false });
   const [arduinoData, setArduinoData] = useState({ counter: 0, textInput: '', valvePWM: 127, motorOn: 0 });
   
@@ -43,22 +47,30 @@ function App() {
   const [isHistorizing, setIsHistorizing] = useState(false);
     
   const [plotState, setPlotState] = useState({data: [], layout: {plotLayout}, frames: [], config: {plotConfig}});
-  
-  socket.emit('arduinoData', JSON5.stringify(arduinoData));
+
+  const [checkboxState, setCheckboxState] = useState(Object.entries(serialData).map((entry, index) => {
+    // give each entry a unique color
+    return {name: entry[0], checked: false, color: `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`};
+  }));
+
+  // Comment out for local testing
+  // socket.emit('arduinoData', JSON5.stringify(arduinoData));
 
   useEffect(() => {
     let UIDebounceInterval;
     let tempSerialData;
 
     // Connect to websocket if not already connected
-    if (!socket || socket.disconnected) {
-      connectToSocket();
-    }
+    // Comment out for local testing
+    // if (!socket || socket.disconnected) {
+    //   connectToSocket();
+    // }
     
     // Handles closing the websocket
     const handleBeforeUnload = (event) => {
       event.preventDefault();
-      socket.disconnect();
+      // Comment out for local testing
+      // socket.disconnect();
       const confirmationMessage = 'Disconnect from websocket?';
       event.returnValue = confirmationMessage; // Standard for most browsers
       return confirmationMessage; // For some older browsers
@@ -69,51 +81,58 @@ function App() {
     
 
     // Receive and parse data from serial port
-    socket.on('serialData', (data) => {
-      try {
-        tempSerialData = JSON5.parse(data);
-        setSerialData(tempSerialData);
-        lastTime = thisTime;
-        thisTime = Number(tempSerialData.timestamp);
-        deltaTime = thisTime - lastTime;
-      } catch (error) {
-        // something went wrong with parsing the data
-      }
-    });
+    // Comment out for local testing
+    // socket.on('serialData', (data) => {
+    //   try {
+    //     tempSerialData = JSON5.parse(data);
+    //     setSerialData(tempSerialData);
+    //     lastTime = thisTime;
+    //     thisTime = Number(tempSerialData.timestamp);
+    //     deltaTime = thisTime - lastTime;
+    //   } catch (error) {
+    //     // something went wrong with parsing the data
+    //   }
+    // });
 
     // update state with parsed data every 50ms
     UIDebounceInterval = setInterval(() => {
-      // setSerialData(tempSerialData);
+      // Comment out for live testing
+      setSerialData(tempSerialData);
 
-      // tempSerialData = {
-      //   timestamp: serialData.timestamp + 100,
-      //   sensor: Math.round(Math.sin(1*serialData.timestamp/1000)*512 + 512),
-      //   lcdButton: Math.round(Math.sin(2*serialData.timestamp/1000)*512 + 512),
-      //   encoderPos: Math.round(Math.sin(3*serialData.timestamp/1000)*512 + 512),
-      //   strainGauge: Math.round(Math.sin(4*serialData.timestamp/1000)*512 + 512),
-      //   pressureTransmitter: Math.round(Math.sin(5*serialData.timestamp/1000)*512 + 512),
-      //   deserializationError: false
-      // }
+      tempSerialData = {
+        timestamp: serialData.timestamp + 100,
+        sensor: Math.round(Math.sin(1*serialData.timestamp/1000)*512 + 512),
+        lcdButton: Math.round(Math.sin(2*serialData.timestamp/1000)*512 + 512),
+        encoderPos: Math.round(Math.sin(3*serialData.timestamp/1000)*512 + 512),
+        strainGauge: Math.round(Math.sin(4*serialData.timestamp/1000)*512 + 512),
+        pressureTransmitter: Math.round(Math.sin(5*serialData.timestamp/1000)*512 + 512),
+        deserializationError: false
+      }
 
-      // setSerialData(tempSerialData);
-      //   lastTime = thisTime;
-      //   thisTime = Number(tempSerialData.timestamp);
-      //   deltaTime = thisTime - lastTime;
+      setSerialData(tempSerialData);
+        lastTime = thisTime;
+        thisTime = Number(tempSerialData.timestamp);
+        deltaTime = thisTime - lastTime;
 
     }, 100);
 
     // Cleanup
     return () => {
-      socket.disconnect();
+      // Comment out for local testing
+      // socket.disconnect();
       clearInterval(UIDebounceInterval);
       window.removeEventListener('beforeunload', handleBeforeUnload);
     };
-  }, []);
+    // Comment out for live testing
+  }, [serialData]);
+    // Comment out for local testing
+  // }, []);
 
 
   // Send data to arduino upon change
   useEffect(() => {
-    socket.emit('arduinoData', JSON5.stringify(arduinoData));
+    // Comment out for local testing
+    // socket.emit('arduinoData', JSON5.stringify(arduinoData));
   }, [arduinoData]);
 
 
@@ -127,24 +146,19 @@ function App() {
   if (isHistorizing) {
     setHistorizedData(h => [...h, serialData]);
     setPlotState({data: [
-      { // trace 1
-        x: historizedData.map((data, index) => {
-          return Number(data.timestamp / 1000);
-        }),
-        y: historizedData.map((data, index) => {
-          return Number(data.strainGauge);
-        }),
-        type: 'scatter',
-        mode: 'lines+markers',
-        marker: {color: 'red'},
-        name: 'Strain gauge'
-      },
-    ], layout: {plotLayout}, frames: [], config: {plotConfig}});
-    }
-    // eslint-disable-next-line
-  }, [serialData, isHistorizing]);
-
-        // { // trace 2
+      // { // trace 1
+      //   x: historizedData.map((data, index) => {
+      //     return Number(data.timestamp / 1000);
+      //   }),
+      //   y: historizedData.map((data, index) => {
+      //     return Number(data.strainGauge);
+      //   }),
+      //   type: 'scatter',
+      //   mode: 'lines+markers',
+      //   marker: {color: 'red'},
+      //   name: 'Strain gauge'
+      // }, 
+      // { // trace 2
       //   x: historizedData.map((data, index) => {
       //     return Number(data.timestamp / 1000);
       //   }),
@@ -155,7 +169,34 @@ function App() {
       //   mode: 'lines+markers',
       //   marker: {color: 'blue'},
       //   name: 'Encoder'
-      // }
+      // },
+      
+      // add a trace for every checked data
+      ...checkboxState.map((entry, index) => {
+        if (entry.checked) {
+          return {
+            x: historizedData.map((data, index) => {
+              return Number(data.timestamp / 1000);
+            }),
+            y: historizedData.map((data, index) => {
+              return Number(data[entry.name]);
+            }),
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: entry.color},
+            name: entry.name
+          };
+        } else {
+          return null;
+        }
+      }).filter((entry) => entry !== null)
+
+
+    ], layout: {plotLayout}, frames: [], config: {plotConfig}});
+    }
+    // eslint-disable-next-line
+  }, [serialData, isHistorizing]);
+
 
   const [catMode, setCatMode] = useState(false);
 
@@ -178,7 +219,8 @@ function App() {
   return (
     <div className="App" style={appStyle}>
       <div style={{position: "absolute", left: "10px"}}>
-        {socket.connected ? (<p>Connected to socket</p>)  : (<p>Not connected to socket</p>)}
+        {/* Comment out for local testing */}
+        {/*socket.connected*/ true ? (<p>Connected to socket</p>)  : (<p>Not connected to socket</p>)}
       </div>
       <div style={{position: "absolute", right: "10px", top: "10px"}}>
         <button onClick={() => setCatMode((e) => !e)}> toggle cat mode </button>
@@ -235,6 +277,32 @@ function App() {
                 <p>Not historizing</p>
               )}
               {historizedData.length > 0 ? <ExportButtonCSV data={historizedData} /> : <></> }
+
+              <fieldset>
+                <legend>Plotted data</legend>
+
+                {/* Map serial data */}
+
+                {checkboxState.map((entry, index) => {
+                  return (
+                    <div key={index}>
+                      <input type="checkbox" id={entry.name} name={entry.name} checked={entry.checked} onChange={() => {
+                        setCheckboxState((state) => {
+                          return state.map((entry) => {
+                            if (entry.name === checkboxState[index].name) {
+                              return {name: entry.name, checked: !entry.checked, color: entry.color};
+                            } else {
+                              return entry;
+                            }
+                          });
+                        });
+                      }} />
+                      <label htmlFor={entry.name}>{entry.name}</label>
+                    </div>
+                  );
+                }
+                )}
+              </fieldset>
               <div>
                 <Plot
                 data={plotState.data}
